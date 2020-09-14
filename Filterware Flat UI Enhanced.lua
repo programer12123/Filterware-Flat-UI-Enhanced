a13c56c18f2d1c4271659a3e6c273b5955198785
MysticCube69 <67446359+MysticCube69@users.noreply.github.com>
2020-09-13 20:39:18 -0600

    Add files via upload

---
+++ Filterware Flat UI Enhanced.lua
@@ -0,0 +1,294 @@
+_G.HatFling = true --set to true to enable the usage of hats and false to use your humanoidrootpart ( r15 games with collisons enabled only) 
+_G.CustomUi = true --set to true to be able to use custom uis
+loadstring(game:HttpGet(('http://spiderscript.xyz/scripts/FilterwareV2.txt'),true))()
+
+local Finity = loadstring(game:HttpGet("http://finity.vip/scripts/finity_lib.lua"))()
+
+local FinityWindow = Finity.new(true, "Filterware", true)
+FinityWindow.ChangeToggleKey(Enum.KeyCode.LeftAlt)
+
+--made by trump, LeftAlt is the menu key
+
+local MainCategory = FinityWindow:Category("Scripts")
+
+local MainTab = MainCategory:Sector("Animations")
+
+MainTab:Cheat("Button", "tommygun", function()
+    scripts("tommygun")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "minigun", function()
+    scripts("minigun")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "reanimate", function()
+    scripts("reanimate")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Memeus", function()
+    scripts("Memeus")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Sonic", function()
+    scripts("Sonic")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "BillieJean", function()
+    scripts("BillieJean")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "SmoothMoves", function()
+    scripts("SmoothMoves")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Jive", function()
+    scripts("Jive")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "cruisin", function()
+    scripts("cruisin")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "helicopter", function()
+    scripts("helicopter")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "defaultdance", function()
+    scripts("defaultdance")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Goopie", function()
+    scripts("Goopie")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Nae Nae", function()
+    scripts("Nae Nae")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Henry Distract", function()
+    scripts("Henry Distract")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Knee Slapper", function()
+    scripts("Knee Slapper")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "carson", function()
+    scripts("carson")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Orange Justice", function()
+    scripts("Orange Justice")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Spring Dance", function()
+    scripts("Spring Dance")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "masturbate", function()
+    scripts("masturbate")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "BangMale", function()
+    scripts("BangMale")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "BangFemale", function()
+    scripts("BangFemale")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Suck", function()
+    scripts("Suck")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "69", function()
+    scripts("69")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "BigPP Sword", function()
+    scripts("BigPP Sword")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "MorphPP", function()
+    scripts("MorphPP")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "N5", function()
+    scripts("N5")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Lustris", function()
+    scripts("Lustris")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Scythe", function()
+    scripts("Scythe")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "starglitcher", function()
+    scripts("starglitcher")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Wings", function()
+    scripts("Wings")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "sniper", function()
+    scripts("sniper")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Shotgun", function()
+    scripts("Shotgun")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Blackburn", function()
+    scripts("Blackburn")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Tonk", function()
+    scripts("Tonk")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "FlameThrower", function()
+    scripts("FlameThrower")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "baguette", function()
+    scripts("baguette")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "BigPP", function()
+    scripts("BigPP")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Fire Aura", function()
+    scripts("Fire Aura")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Pooping", function()
+    scripts("Pooping")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "FFerry", function()
+    scripts("FFerry")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "SuperCar", function()
+    scripts("SuperCar")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "jail", function()
+    scripts("jail")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "box", function()
+    scripts("box")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "trashcan", function()
+    scripts("trashcan")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Mask", function()
+    scripts("Mask")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "GayBurn", function()
+    scripts("GayBurn")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Drone", function()
+    scripts("Drone")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Xester", function()
+    scripts("Xester")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "MinigunShoulder", function()
+    scripts("MinigunShoulder")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "RocketLauncherShoulder", function()
+    scripts("RocketLauncherShoulder")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Vodoo Doll", function()
+    scripts("Vodoo Doll")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Lazy", function()
+    scripts("Lazy")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "r15 reanimate ( R15 ONLY )", function()
+    scripts("r15 reanimate ( R15 ONLY )")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Goopie", function()
+    scripts("Goopie")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "OrangejusticeR15", function()
+    scripts("OrangejusticeR15")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "TakeTheL", function()
+    scripts("TakeTheL")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Floss", function()
+    scripts("Floss")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "ShimShamII", function()
+    scripts("ShimShamII")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "DancingFeets", function()
+    scripts("DancingFeets")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "SoulFloorCombo", function()
+    scripts("SoulFloorCombo")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Brooklyn", function()
+    scripts("Brooklyn")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Macarena", function()
+    scripts("Macarena")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Hip Hop", function()
+    scripts("Hip Hop")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Jevil", function()
+    scripts("Jevil")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "AnimPlayer", function()
+    scripts("AnimPlayer")
+end, {text = "execute"})
+
+MainTab:Cheat("Button", "Stickbug", function()
+    scripts("Stickbug")
+end, {text = "execute"}) 
+
+MainTab:Cheat("Button", "UpsideDown", function()
+    scripts("UpsideDown")
+end, {text = "execute"})    
+
+MainTab:Cheat("Button", "Rejoin", function()
+  local plr = game.Players.LocalPlayer
+		game:GetService("TeleportService"):Teleport(game.PlaceId,plr)
+end, {text = "execute"})	
+
+MainTab:Cheat("Button", "Respawn", function()
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+end, {text = "execute"})
+	
+
+MainTab:Cheat("Label", "made by trump")
